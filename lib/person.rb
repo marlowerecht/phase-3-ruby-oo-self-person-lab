c2d68b3951a0dd18bf353805ce3a05db83412b21
Marlowe Recht <recht.marlowe@gmail.com>
2023-01-31 17:32:03 +0100

    commit tree 17c9995d01afd965dcd6d2ed3c9dc0a7b2cd73c0

--- lib/person.rb
+++ lib/person.rb
@@ -1,1 +1,87 @@
-# your code goes here
+require 'pry'
+
+class Person
+
+    attr_reader :name, :bank_account, :happiness, :hygiene
+    attr_writer :bank_account
+
+    def initialize(name, bank_account = 25, happiness = 8, hygiene = 8)
+        @name = name
+        @bank_account = bank_account
+        @happiness = happiness
+        @hygiene = hygiene
+    end
+
+    def happiness=(new_happiness)
+        if new_happiness < 0
+            @happiness = 0
+        elsif new_happiness > 10
+            @happiness = 10
+        else
+            @happiness = new_happiness
+        end
+    end
+
+    def hygiene=(new_hygiene)
+        if new_hygiene < 0
+            @hygiene = 0
+        elsif new_hygiene > 10
+            @hygiene = 10
+        else
+            @hygiene = new_hygiene
+        end
+    end
+
+    def clean?
+        if self.hygiene > 7
+            true
+        else
+            false
+        end
+    end
+
+    def happy?
+        if self.happiness > 7
+            true
+        else
+            false
+        end
+    end
+
+    def get_paid(amount)
+        self.bank_account += amount
+        "all about the benjamins"
+    end
+
+    def take_bath
+        self.hygiene += 4
+        "♪ Rub-a-dub just relaxing in the tub ♫"
+    end
+
+    def work_out
+        self.happiness += 2
+        self.hygiene -= 3
+        "♪ another one bites the dust ♫"
+    end
+
+    def call_friend(friend)
+        self.happiness += 3
+        friend.happiness += 3
+        "Hi #{friend.name}! It's #{self.name}. How are you?"
+    end
+
+    def start_conversation(friend, topic)
+        if topic == "politics"
+            self.happiness -= 2
+            friend.happiness -= 2
+            "blah blah partisan blah lobbyist"
+        elsif topic == "weather"
+            self.happiness += 1
+            friend.happiness += 1
+            "blah blah sun blah rain"
+        else
+            "blah blah blah blah blah"
+        end
+    end
+
+end
